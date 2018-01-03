92a52472f46bd29508ee6f893606a30491bccd65
John Wiseheart <johnwiseheart@gmail.com>
2018-01-02 18:48:08 -0800

    initial dist subtree commit

---
+++ packages/server/dist/common/types.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var UnitType;
+(function (UnitType) {
+    UnitType["SI"] = "si";
+    UnitType["US"] = "us";
+})(UnitType = exports.UnitType || (exports.UnitType = {}));
+//# sourceMappingURL=types.js.map
