3e4983ea4a77f3a27dbdfdeeb91090d5245731f9
johnwiseheart <johnwiseheart@gmail.com>
2018-01-10 22:38:54 -0800

    simplify deploy script

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -1,16 +1,10 @@
-rm -rf _build
-mkdir _build
-
-git clone https://${GH_TOKEN}@github.com/johnwiseheart/whereis.git _build
-
-cd _build
-
-yarn
 yarn build-app
 
 git push -d origin gh-pages
 
+echo "whereis.jcaw.me" > packages/app/dist/CNAME
+
 git add packages/app/dist -f
 git commit --allow-empty -m "Commiting built assets"
-git subtree push --prefix packages/app/dist origin gh-pages
+git subtree push --prefix packages/app/dist https://${GH_TOKEN}@github.com/johnwiseheart/whereis.git origin gh-pages
 
