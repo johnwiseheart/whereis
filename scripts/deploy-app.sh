dcb09f318b9fd0752a983ae52bdfc6a587afd11a
johnwiseheart <johnwiseheart@gmail.com>
2018-01-10 23:08:30 -0800

    More cleanup

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -1,12 +1,11 @@
-yarn build-app
-
-GITHUB_URL="https://${GH_TOKEN}@github.com/johnwiseheart/whereis.git"
+REPO_NAME=`git config --get remote.origin.url | cut -d':' -f2`
+GITHUB_URL="https://${GH_TOKEN}@github.com/${REPO_NAME}"
 
 git push -d $GITHUB_URL gh-pages
 
-echo "whereis.jcaw.me" > packages/app/dist/CNAME
+echo $FRONTEND_URL > packages/app/dist/CNAME
 
 git add packages/app/dist -f
-git commit --allow-empty -m "Commiting built assets"
+git commit --allow-empty -m "Commiting built assets for gh-pages"
 git subtree push --prefix packages/app/dist $GITHUB_URL gh-pages
 
