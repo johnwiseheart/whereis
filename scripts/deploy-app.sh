7bcaa737851a3c63b7e7d92cf26189d5d2f22916
johnwiseheart <johnwiseheart@gmail.com>
2018-01-10 22:45:18 -0800

    Trying to fix it up a little

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -1,10 +1,12 @@
 yarn build-app
 
-git push -d origin gh-pages
+GITHUB_URL="https://${GH_TOKEN}@github.com/johnwiseheart/whereis.git"
+
+git push URL -d origin gh-pages
 
 echo "whereis.jcaw.me" > packages/app/dist/CNAME
 
 git add packages/app/dist -f
 git commit --allow-empty -m "Commiting built assets"
-git subtree push --prefix packages/app/dist https://${GH_TOKEN}@github.com/johnwiseheart/whereis.git origin gh-pages
+git subtree push --prefix packages/app/dist GITHUB_URL origin gh-pages
 
