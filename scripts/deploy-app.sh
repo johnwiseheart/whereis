eddeae7e5d009af7aac0ed963db6556557710d1c
johnwiseheart <johnwiseheart@gmail.com>
2018-01-10 22:49:32 -0800

    Third time is the charm

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -2,11 +2,11 @@ yarn build-app
 
 GITHUB_URL="https://${GH_TOKEN}@github.com/johnwiseheart/whereis.git"
 
-git push URL -d origin gh-pages
+git push $GITHUB_URL -d origin gh-pages
 
 echo "whereis.jcaw.me" > packages/app/dist/CNAME
 
 git add packages/app/dist -f
 git commit --allow-empty -m "Commiting built assets"
-git subtree push --prefix packages/app/dist GITHUB_URL origin gh-pages
+git subtree push --prefix packages/app/dist $GITHUB_URL gh-pages
 
