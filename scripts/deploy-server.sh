b7d20fe7b26e1537dfab88e614ce76dc7052a76f
johnwiseheart <johnwiseheart@gmail.com>
2018-01-11 00:16:36 -0800

    Move build step from dokku to travis

--- scripts/deploy-server.sh
+++ scripts/deploy-server.sh
@@ -1,8 +1,9 @@
-ls packages/server/dist
-
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
+
+git add packages/server/dist -f
+git commit --allow-empty -m "Building for dokku"
 
 git remote add dokku "dokku@dynamic.jcaw.me:${DOKKU_APPNAME}"
 git push dokku master
