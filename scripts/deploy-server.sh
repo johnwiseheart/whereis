6427c24e4bffaa41ad1d4f2b57343d139343927b
johnwiseheart <johnwiseheart@gmail.com>
2018-01-11 00:25:06 -0800

    Trying out other methods

--- scripts/deploy-server.sh
+++ scripts/deploy-server.sh
@@ -2,8 +2,11 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
-git add packages/server/dist -f
+rm .gitignore
+git add packages/server/dist
 git commit --allow-empty -m "Building for dokku"
+
+git log
 
 git remote add dokku "dokku@dynamic.jcaw.me:${DOKKU_APPNAME}"
 git push dokku master
