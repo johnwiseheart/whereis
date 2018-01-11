2c437372c78f2a50a4c77772f2cdc32cd76d0637
johnwiseheart <johnwiseheart@gmail.com>
2018-01-11 00:00:45 -0800

    Fix ssh key issues

--- scripts/deploy-server.sh
+++ scripts/deploy-server.sh
@@ -1,3 +1,7 @@
 ls packages/server/dist
 
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
+
 ssh dokku@dynamic.jcaw.me
