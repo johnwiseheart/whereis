ecb4ab4ac026a47d7c0c1e5258968d0071a1caf0
johnwiseheart <johnwiseheart@gmail.com>
2020-04-18 17:18:16 +0000

    Deploying to gh-pages from  @ 64937a7bcfa29ac627a1bc4b80473fbc04319d3a 🚀

---
+++ precache-manifest.9a6ef99adfe92d918d8883a36b0143c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be1ae3526b9e779a272e6c66dcb5ac8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e4ebcebd09321f844f9",
+    "url": "/static/css/main.8c84ba3e.chunk.css"
+  },
+  {
+    "revision": "a692773a9c089a3bfc01",
+    "url": "/static/js/2.248e04d8.chunk.js"
+  },
+  {
+    "revision": "9e4ebcebd09321f844f9",
+    "url": "/static/js/main.82b8853d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
